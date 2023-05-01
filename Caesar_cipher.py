b9ea4aa4e52743d315bd1a65e3676b10b7696787
YIsungjoon <leehm21897@gmail.com>
2023-05-01 17:21:00 +0900

    feat: Create ceaser_cipher function.

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -1,1 +1,8 @@
-print("hello world")
+def ceaser_cipher(text, n):
+    result = ""
+    for st in text:
+        result += chr(ord(st)+n)
+    return result
+
+if __name__ == "__main__":
+    print(ceaser_cipher("hello", 3))
